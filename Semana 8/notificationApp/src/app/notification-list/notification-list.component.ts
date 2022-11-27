e8d41319e6bd0aed63a05113fe6525875354c952
RuanUFSC <ruaanlopes@hotmail.com>
2022-11-27 18:44:09 -0300

    [M1S08] Ex 7 - Valores Typescript

--- Semana 8/notificationApp/src/app/notification-list/notification-list.component.ts
+++ Semana 8/notificationApp/src/app/notification-list/notification-list.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./notification-list.component.scss']
 })
 export class NotificationListComponent {
-
+  notifications = [
+    { 
+      title: 'Instagram',
+      desc:'Elon Musk curtiu sua publicação',
+      time:'1 min ago'
+    },
+    {  
+      title: 'WhatsApp',
+      desc:'Mãe: Tudo bem?',
+      time:'5 min ago'
+    },
+    {  
+      title: 'Linkedin',
+      desc:'3 empregos encontrados em Worldwide',
+      time:'7 min ago'
+    },
+    {  
+      title: 'Linkedin',
+      desc:'1 emprego encontrado em Biguacu',
+      time:'10 min ago'
+    },
+    {  
+      title: 'Linkedin',
+      desc:'2 empregos encontrados em Florianopolis',
+      time:'12 min ago'
+    }
+  ]
 }
