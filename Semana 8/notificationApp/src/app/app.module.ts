e3731ed5c76a7a9835f2d3914c4f803eb7e223a0
RuanUFSC <ruaanlopes@hotmail.com>
2022-12-12 10:42:13 -0300

    [M1S10] Ex 2 - Criação do componente de filtro

--- Semana 8/notificationApp/src/app/app.module.ts
+++ Semana 8/notificationApp/src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NotificationListComponent } from './notification-list/notification-list.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { NotificationListComponent } from './pages/notification-list/notification-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FilterComponent } from './components/filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotificationListComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule
