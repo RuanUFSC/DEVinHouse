470299269f41e802ae07772124bf6d369213e6c5
RuanUFSC <ruaanlopes@hotmail.com>
2023-01-21 21:47:59 -0300

    Ex. Indica Filmes com ngSwitch

--- Semana 11/app-10/src/app/app.component.ts
+++ Semana 11/app-10/src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  clicou:boolean = false;
-  classes = {'clicked': this.clicou};
-  changeValor() {
-    this.clicou = !this.clicou;
-    this.classes = {'clicked': this.clicou};
-  };
+  inputValue: string = '';
 }
