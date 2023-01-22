96ce046f353eb0c14cd5181186d4c0c99c394828
RuanUFSC <ruaanlopes@hotmail.com>
2023-01-21 21:59:18 -0300

    Ex. Misturando ngFor com ngSwitch

--- Semana 11/app-10/src/app/app.component.ts
+++ Semana 11/app-10/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  inputValue: string = '';
+  opcaoSelecionada: string = '';
+  selectedOption: string = '';
+  generos = ['acao', 'comedia', 'aventura', 'aleatorio']
 }
