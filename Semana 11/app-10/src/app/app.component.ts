3fe9ff068a90bec3457d7663ea9e839ed7c9608b
RuanUFSC <ruaanlopes@hotmail.com>
2023-01-22 20:59:49 -0300

    [M1S11] Ex 4 - ngIf

--- Semana 11/app-10/src/app/app.component.ts
+++ Semana 11/app-10/src/app/app.component.ts
@@ -6,26 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  arrayFotos: any = [
-    {
-       url: "https://cdn.lorem.space/images/book/.cache/500x0/a-brief-history-of-time.jpeg",
-       description:"livros"
-     },
-     {
-       url: "https://cdn.lorem.space/images/movie/.cache/500x0/the-martian-2015.jpeg",
-       description:"filmes"
-     },
-     {
-       url: "https://cdn.lorem.space/images/game/.cache/500x0/los-1.jpg",
-       description:"games"
-     },
-     {
-       url: "https://cdn.lorem.space/images/face/.cache/500x0/omer-karakus-AjGyA81epeI-unsplash.jpg",
-       description:"rosto"
-     },
-     {
-       url: "https://cdn.lorem.space/images/shoes/.cache/500x0/alexander-rotker-l8p1aWZqHvE-unsplash.jpg",
-       description:"sapato"
-     }
-   ]
+  showModal: boolean = false;
 }
