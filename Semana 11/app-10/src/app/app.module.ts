a3f3db11cd3fbc9ce95dfc3b6d290e6150e4325d
RuanUFSC <ruaanlopes@hotmail.com>
2023-01-22 22:33:57 -0300

    [M1S11] Ex 8 - Revisão geral

--- Semana 11/app-10/src/app/app.module.ts
+++ Semana 11/app-10/src/app/app.module.ts
@@ -8,7 +8,8 @@ import { CaptalizePhillipsPipe } from './pipes/captalize-phillips.pipe';
 import { CpfPipe } from './pipes/cpf.pipe';
 import { ModalComponent } from './components/modal/modal.component';
 import { CardExemploComponent } from './components/card-exemplo/card-exemplo.component';
-import { FormValidacaoComponent } from './components/form-validacao/form-validacao.component'
+import { FormValidacaoComponent } from './components/form-validacao/form-validacao.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component'
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { FormValidacaoComponent } from './components/form-validacao/form-validac
     CpfPipe,
     ModalComponent,
     CardExemploComponent,
-    FormValidacaoComponent
+    FormValidacaoComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
