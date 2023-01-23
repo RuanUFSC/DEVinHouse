eec8de36dc564101498ab1a4e7145aa308be085a
RuanUFSC <ruaanlopes@hotmail.com>
2023-01-22 21:14:15 -0300

    [M1S11] Ex 5 - ng-content

--- Semana 11/app-10/src/app/app.module.ts
+++ Semana 11/app-10/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CaptalizePhillipsPipe } from './pipes/captalize-phillips.pipe';
-import { CpfPipe } from './pipes/cpf.pipe'
+import { CpfPipe } from './pipes/cpf.pipe';
+import { ModalComponent } from './components/modal/modal.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     CaptalizePhillipsPipe,
-    CpfPipe
+    CpfPipe,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
